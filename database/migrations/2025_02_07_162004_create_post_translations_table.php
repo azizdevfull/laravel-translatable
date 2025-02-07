50baf23eb74e28e62c339c91e3fc3dfc1937783e
Azizbek Isroilov <aziz16110904@gmail.com>
2025-02-07 21:26:24 +0500

    Test part completed

--- database/migrations/2025_02_07_162004_create_post_translations_table.php
+++ database/migrations/2025_02_07_162004_create_post_translations_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('title');
             $table->text('content')->nullable();
             $table->unique(['post_id', 'locale']);
-            $table->timestamps();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $post = Post::find(1);
+
+    // $post->translations()->createMany([
+    //     ['locale' => 'en', 'title' => 'Laravel Tips', 'content' => 'Laravel is awesome!'],
+    //     ['locale' => 'uz', 'title' => 'Laravel Maslahatlar', 'content' => 'Laravel juda zo‘r!'],
+    // ]);
+    return $post->translation()->title;
 });
